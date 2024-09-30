f21507784ab6cebc274916f03d1030200f471b5a
Nik Graf <nik@nikgraf.com>
2024-09-30 17:32:39 +0200

    improve ui

--- apps/events/src/components/schema-test-yjs.tsx
+++ apps/events/src/components/schema-test-yjs.tsx
@@ -1,12 +1,23 @@
+import { Input } from "@/components/ui/input";
 import { createSchema, t } from "@/query/graph-lib";
-import React, { useState } from "react";
+import { CalendarDays, MapPin } from "lucide-react";
+import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import * as Yjs from "yjs";
+import { Button } from "./ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 
 const schema = {
   entities: {
     user: { id: t.String, name: t.String },
-    events: { id: t.String, title: t.String, attendees: t.Number },
+    event: { id: t.String, title: t.String, attendees: t.Number },
   },
 } as const;
 
@@ -19,47 +30,81 @@ export const SchemaTestYjs: React.FC = () => {
     yDoc,
   });
 
-  const events = useQuery({ entity: "events" });
+  useEffect(() => {
+    createEntity({
+      entity: "user",
+      data: { id: uuidv4(), name: "Jane" },
+    });
+  }, []);
+
+  const events = useQuery({ entity: "event" });
+  const users = useQuery({ entity: "user" });
 
   // local state for the text of a new event
   const [newEventTitle, setNewEventTitle] = useState("");
 
   return (
-    <div>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          createEntity({
-            entity: "events",
-            data: { id: uuidv4(), title: newEventTitle, attendees: 0 },
-          });
-          setNewEventTitle("");
-        }}
-      >
-        <input
-          placeholder="New Event Title"
-          onChange={(event) => setNewEventTitle(event.target.value)}
-          value={newEventTitle}
-        />
-        <button className="add">Add</button>
-      </form>
+    <main className="flex-1">
+      <section className="w-full py-12 md:py-24">
+        <div className="container px-4 md:px-6">
+          <h1>{users[0] ? `${users[0].name}'s Events` : ""}</h1>
 
-      <ul>
-        {events.map((event) => {
-          return (
-            <li key={event.id}>
-              <div>{event.title}</div>
-              <button
-                onClick={() => {
-                  deleteEntity({ entity: "events", id: event.id });
-                }}
-              >
-                x
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              createEntity({
+                entity: "event",
+                data: { id: uuidv4(), title: newEventTitle, attendees: 0 },
+              });
+              setNewEventTitle("");
+            }}
+          >
+            <Input
+              placeholder="New Event Title"
+              onChange={(event) => setNewEventTitle(event.target.value)}
+              value={newEventTitle}
+            />
+            <Button className="add">Add</Button>
+          </form>
+
+          <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+            {events.map((event) => {
+              return (
+                <Card key={event.id}>
+                  <CardHeader>
+                    <CardTitle>{event.title}</CardTitle>
+                    <CardDescription>
+                      <div className="flex items-center mt-2">
+                        <CalendarDays className="mr-2 h-4 w-4" />
+                        {
+                          // @ts-expect-error tinybase types issue
+                          new Date(event.date).toLocaleDateString()
+                        }
+                      </div>
+                      <div className="flex items-center mt-2">
+                        <MapPin className="mr-2 h-4 w-4" />
+                        New York, NY
+                      </div>
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <p>A new event</p>
+                  </CardContent>
+                  <CardFooter className="flex gap-2">
+                    <Button
+                      onClick={() => {
+                        deleteEntity({ entity: "event", id: event.id });
+                      }}
+                    >
+                      Delete
+                    </Button>
+                  </CardFooter>
+                </Card>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </main>
   );
 };
--- apps/events/src/routes/schema.tsx
+++ apps/events/src/routes/schema.tsx
@@ -2,10 +2,5 @@ import { SchemaTestYjs } from "@/components/schema-test-yjs";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/schema")({
-  component: () => (
-    <div className="flex flex-1 justify-center items-center flex-col gap-4">
-      {/* <SchemaTestAutomerge /> */}
-      <SchemaTestYjs />
-    </div>
-  ),
+  component: () => <SchemaTestYjs />,
 });
