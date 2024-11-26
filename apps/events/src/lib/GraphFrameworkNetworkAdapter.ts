4359745683eb9fa48d939cdb43f1e0a1c8fe4fbb
Nik Graf <nik@nikgraf.com>
2024-11-26 15:33:36 +0100

    wip

--- apps/events/src/lib/GraphFrameworkNetworkAdapter.ts
+++ apps/events/src/lib/GraphFrameworkNetworkAdapter.ts
@@ -2,7 +2,10 @@ import { uuid } from '@automerge/automerge';
 import { NetworkAdapter } from '@automerge/automerge-repo';
 import type { Message, PeerId, PeerMetadata } from '@automerge/automerge-repo/slim';
 import { hexToBytes } from '@noble/hashes/utils';
-import { type RequestCreateUpdate, encryptMessage, serialize } from 'graph-framework';
+import * as Schema from 'effect/Schema';
+import { type RequestCreateUpdate, ResponseMessage, deserialize, encryptMessage, serialize } from 'graph-framework';
+
+const decodeResponseMessage = Schema.decodeUnknownEither(ResponseMessage);
 
 export class GraphFrameworkNetworkAdapter extends NetworkAdapter {
   webSocket?: WebSocket;
@@ -12,6 +15,7 @@ export class GraphFrameworkNetworkAdapter extends NetworkAdapter {
 
   setWebSocket(websocket: WebSocket) {
     this.webSocket = websocket;
+    this.webSocket.addEventListener('message', this.onMessage);
   }
 
   setSpaceValues({
@@ -67,6 +71,60 @@ export class GraphFrameworkNetworkAdapter extends NetworkAdapter {
       spaceId: this.spaceId,
     };
     this.webSocket.send(serialize(messageToSend));
+  }
+
+  async onMessage(event: MessageEvent) {
+    const data = deserialize(event.data);
+    const message = decodeResponseMessage(data);
+    if (message._tag === 'Right') {
+      const response = message.right;
+      switch (response.type) {
+        case 'space': {
+          break;
+        }
+        case 'update-confirmed': {
+          // setSpaces((spaces) =>
+          //   spaces.map((space) => {
+          //     if (space.id === response.spaceId && space.lastUpdateClock + 1 === response.clock) {
+          //       return { ...space, lastUpdateClock: response.clock };
+          //     }
+          //     return space;
+          //   }),
+          // );
+          // setUpdatesInFlight((updatesInFlight) => updatesInFlight.filter((id) => id !== response.ephemeralId));
+          break;
+        }
+        case 'updates-notification': {
+          // setSpaces((spaces) =>
+          //   spaces.map((space) => {
+          //     if (space.id === response.spaceId) {
+          //       let lastUpdateClock = space.lastUpdateClock;
+          //       if (response.updates.firstUpdateClock === space.lastUpdateClock + 1) {
+          //         lastUpdateClock = response.updates.lastUpdateClock;
+          //       } else {
+          //         // TODO request missing updates from server
+          //       }
+
+          //       const newUpdates = (response.updates ? response.updates.updates : []).map((encryptedUpdate) => {
+          //         return decryptMessage({
+          //           nonceAndCiphertext: encryptedUpdate,
+          //           secretKey: hexToBytes(space.keys[0].key),
+          //         });
+          //       });
+
+          //       return {
+          //         ...space,
+          //         updates: [...space.updates, ...newUpdates],
+          //         lastUpdateClock,
+          //       };
+          //     }
+          //     return space;
+          //   }),
+          // );
+          break;
+        }
+      }
+    }
   }
 
   disconnect() {
