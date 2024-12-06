eb3cf2da5c61107c0df0c4231f173f9eac017550
Nik Graf <nik@nikgraf.com>
2024-12-06 11:11:14 +0100

    ids can have a length of 21 or 22

--- packages/graph-framework-utils/test/generateId.test.ts
+++ packages/graph-framework-utils/test/generateId.test.ts
@@ -5,5 +5,5 @@ import { generateId } from '../src/generateId.js';
 it('should generate a base58 encoded uuid of 22 char length', () => {
   const id = generateId();
   expect(id).toBeTypeOf('string');
-  expect(id).toHaveLength(22);
+  expect(id.length === 21 || id.length === 22).toBe(true);
 });
