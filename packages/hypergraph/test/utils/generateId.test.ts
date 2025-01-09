14d720e72eab1fd1af44acb00e8d2da49f62e11a
Chris Whited <cmwhited1990@gmail.com>
2025-01-09 11:49:34 -1000

    chore(consolidate packages): move utils tests into hypergraph

---
+++ packages/hypergraph/test/utils/generateId.test.ts
@@ -0,0 +1,9 @@
+import { expect, it } from 'vitest';
+
+import { generateId } from '../../src/utils/generateId.js';
+
+it('utils/should generate a base58 encoded uuid of 22 char length', () => {
+  const id = generateId();
+  expect(id).toBeTypeOf('string');
+  expect(id.length === 21 || id.length === 22).toBe(true);
+});
---
+++ packages/hypergraph/test/utils/stringToUint8Array.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, it } from 'vitest';
+
+import { stringToUint8Array } from '../../src/utils/stringToUint8Array.js';
+
+describe('utils/stringToUint8Array', () => {
+  // will fix later
+  it.skip('should convert a string to a Uint8Array', () => {
+    const encoded = stringToUint8Array('Hello World');
+    expect(encoded).toEqual(new Uint8Array([72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100]));
+
+    const encoded2 = stringToUint8Array('{ "entry": "Hello World" }');
+    expect(encoded2).toEqual(
+      new Uint8Array([
+        123, 32, 34, 101, 110, 116, 114, 121, 34, 58, 32, 34, 72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 34,
+        32, 125,
+      ]),
+    );
+  });
+});
