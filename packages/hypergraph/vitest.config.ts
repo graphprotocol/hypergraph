e33439512d957e4acc46036c4658aa867f6c8282
Nik Graf <nik@nikgraf.com>
2025-01-22 08:05:08 +0100

    cleanup deps

--- packages/hypergraph/vitest.config.ts
+++ packages/hypergraph/vitest.config.ts
@@ -1,12 +1,6 @@
-import react from '@vitejs/plugin-react';
 import { type UserConfigExport, mergeConfig } from 'vitest/config';
 import shared from '../../vitest.shared.js';
 
-const config: UserConfigExport = {
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-  },
-};
+const config: UserConfigExport = {};
 
 export default mergeConfig(shared, config);
