e60606c575898802a8e987306bcf6dd2c8b787e7
Pablo Carranza Velez <pablo@edgeandnode.com>
2025-01-23 08:50:41 -0300

    chore: add some comments from PR review

--- packages/hypergraph/src/identity/identity-encryption.ts
+++ packages/hypergraph/src/identity/identity-encryption.ts
@@ -16,6 +16,16 @@ const hkdfDeriveKey = (secret: Uint8Array, salt: Uint8Array): Uint8Array => {
   return hkdf(sha256, secret, salt, '', 32);
 };
 
+// This implements the same encryption as  https://github.com/xmtp/xmtp-js/blob/336471de4ea95416ad0f4f9850d3f12bb0a13f1e/sdks/js-sdk/src/encryption/encryption.ts#L18
+// But using @noble/ciphers instead of the WebCrypto API.
+// The XMTP code was audited by Certik: https://skynet.certik.com/projects/xmtp
+//
+// Worth noting that GCM nonce collision would break the encryption,
+// and 12 bytes is not a lot. So this function should not be used to encrypt
+// a large number of messages with the same secret. In our case it should be okay
+// as each secret is only used to encrypt a single identity. If we need
+// something more secure for a larger number of messages we should use a
+// different encryption scheme, e.g. XAES-256-GCM, see https://words.filippo.io/dispatches/xaes-256-gcm/
 const encrypt = (msg: Uint8Array, secret: Uint8Array): string => {
   const hkdfSalt = randomBytes(32);
   const gcmNonce = randomBytes(12);
@@ -36,6 +46,9 @@ const encrypt = (msg: Uint8Array, secret: Uint8Array): string => {
   return bytesToHex(new TextEncoder().encode(ciphertextJson));
 };
 
+// This implements the same decryption as  https://github.com/xmtp/xmtp-js/blob/336471de4ea95416ad0f4f9850d3f12bb0a13f1e/sdks/js-sdk/src/encryption/encryption.ts#L41
+// But using @noble/ciphers instead of the WebCrypto API
+// The XMTP code was audited by Certik: https://skynet.certik.com/projects/xmtp
 const decrypt = (ciphertext: string, secret: Uint8Array): Uint8Array => {
   const ciphertextJson = new TextDecoder().decode(hexToBytes(ciphertext));
   const { aes256GcmHkdfSha256 } = JSON.parse(ciphertextJson);
