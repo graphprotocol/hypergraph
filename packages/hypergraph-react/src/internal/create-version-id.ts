619cad8c5b692fa6b6832307d198c0d06920af47
Nik Graf <nik@nikgraf.com>
2025-04-22 16:44:05 +0200

    fix md5 hashing for web platform

--- packages/hypergraph-react/src/internal/create-version-id.ts
+++ packages/hypergraph-react/src/internal/create-version-id.ts
@@ -1,25 +1,12 @@
-// biome-ignore lint/style/useNodejsImportProtocol: using a package
-import { createHash } from 'crypto';
-// needed to fix sorting issues
 import { Base58 } from '@graphprotocol/grc-20';
+import { md5 } from '@noble/hashes/legacy';
 import { v4 } from 'uuid';
 
 function createIdFromUniqueString(text: string) {
-  const hashed = createHash('md5').update(text).digest('hex');
-  const bytes = hexToBytesArray(hashed);
-  // @ts-expect-error
-  const uuid = v4({ random: bytes });
+  const encoded = new TextEncoder().encode(text);
+  const hashed = md5(encoded);
+  const uuid = v4({ random: hashed });
   return Base58.encodeBase58(uuid.split('-').join(''));
-}
-
-function hexToBytesArray(hex: string) {
-  const bytes: number[] = [];
-
-  for (let character = 0; character < hex.length; character += 2) {
-    bytes.push(Number.parseInt(hex.slice(character, character + 2), 16));
-  }
-
-  return bytes;
 }
 
 export function createVersionId({ proposalId, entityId }: { proposalId: string; entityId: string }): string {
