1a4a715079c72ba6b21631c7cdd346c6e2453d6b
Marcus Rein <marcus.rein@gmail.com>
2025-06-10 08:40:33 -0400

    docs: update Docusaurus color mode configuration to disable theme switch

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      colorMode: { defaultMode: 'dark' },
+      colorMode: { defaultMode: 'dark', disableSwitch: true },
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
