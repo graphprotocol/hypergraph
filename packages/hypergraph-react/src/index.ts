1c7c7208add51e695717cc8c8b0e6135220ce146
Nik Graf <nik@nikgraf.com>
2025-06-23 14:11:37 +0200

    add createEntityPublic

--- packages/hypergraph-react/src/index.ts
+++ packages/hypergraph-react/src/index.ts
@@ -23,6 +23,7 @@ export {
   useUpdateEntity,
 } from './HypergraphSpaceContext.js';
 export { generateDeleteOps as _generateDeleteOps } from './internal/generate-delete-ops.js';
+export { useCreateEntityPublic as _useCreateEntityPublic } from './internal/use-create-entity-public.js';
 export { useDeleteEntityPublic as _useDeleteEntityPublic } from './internal/use-delete-entity-public.js';
 export { useGenerateCreateOps as _useGenerateCreateOps } from './internal/use-generate-create-ops.js';
 export { useQueryPublic as _useQueryPublic } from './internal/use-query-public.js';
