81dbd36a2e30810737f263a1a41d1f745b8dfb41
Nik Graf <nik@nikgraf.com>
2025-06-26 13:10:53 +0200

    Ng/fix private spaces (#261)

--- packages/hypergraph/src/identity/prove-ownership.ts
+++ packages/hypergraph/src/identity/prove-ownership.ts
@@ -64,13 +64,13 @@ export const verifyIdentityOwnership = async (
     chain,
     transport: http(rpcUrl),
   });
-  console.log('publicClient', publicClient);
-  console.log('rpcUrl', rpcUrl);
-  console.log('chain', chain);
+  // console.log('publicClient', publicClient);
+  // console.log('rpcUrl', rpcUrl);
+  // console.log('chain', chain);
 
-  console.log('accountProof', accountProof);
-  console.log('accountAddress', accountAddress);
-  console.log('publicKey', publicKey);
+  // console.log('accountProof', accountProof);
+  // console.log('accountAddress', accountAddress);
+  // console.log('publicKey', publicKey);
 
   const accountProofMessage = getAccountProofMessage(accountAddress, publicKey);
   const validAccountProof = await publicClient.verifyTypedData({
