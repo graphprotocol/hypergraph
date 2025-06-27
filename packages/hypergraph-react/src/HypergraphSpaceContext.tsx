d78a3690ef5b1dac4c2f4438d9bcde46f1d0bd0f
Nik Graf <nik@nikgraf.com>
2025-06-27 18:54:24 +0200

    avoid throwing in case no hypergraph space provider exists

--- packages/hypergraph-react/src/HypergraphSpaceContext.tsx
+++ packages/hypergraph-react/src/HypergraphSpaceContext.tsx
@@ -15,16 +15,13 @@ import {
 } from 'react';
 import { useHypergraphApp } from './HypergraphAppContext.js';
 
+// TODO space can be undefined
 export type HypergraphContext = { space: string };
 
 export const HypergraphReactContext = createContext<HypergraphContext | undefined>(undefined);
 
 export function useHypergraphSpaceInternal() {
   const context = useContext(HypergraphReactContext);
-  if (!context) {
-    throw new Error('useHypergraphSpace must be used within a HypergraphSpaceProvider');
-  }
-
   return context as HypergraphContext;
 }
 
