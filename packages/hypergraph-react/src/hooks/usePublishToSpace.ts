d1feaa414479c2710c0bf7fbee641d6304244335
Byron Guina <byron.guina@gmail.com>
2025-07-30 16:04:02 -0700

    feat: move generic to mutationFn

--- packages/hypergraph-react/src/hooks/usePublishToSpace.ts
+++ packages/hypergraph-react/src/hooks/usePublishToSpace.ts
@@ -4,11 +4,11 @@ import { useHypergraphApp } from '../HypergraphAppContext.js';
 import { preparePublish } from '../prepare-publish.js';
 import { publishOps } from '../publish-ops.js';
 
-export function usePublishToPublicSpace<S extends Entity.AnyNoContext>(spaceId: string) {
+export function usePublishToPublicSpace(spaceId: string) {
   const { getSmartSessionClient } = useHypergraphApp();
 
   return useMutation({
-    mutationFn: async (entity: Entity.Entity<S>) => {
+    mutationFn: async <S extends Entity.AnyNoContext>(entity: Entity.Entity<S>) => {
       const { ops } = await preparePublish({
         entity,
         publicSpace: spaceId,
