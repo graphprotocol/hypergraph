ea23c185a3aca8265a8fca7d397484f994409cc7
Hashir <mail2hashirpm@gmail.com>
2025-07-30 12:55:40 +0530

    fix: remove favicon path from Docusaurus config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -12,7 +12,6 @@ import { themes as prismThemes } from 'prism-react-renderer';
 const config = {
   title: 'Hypergraph',
   tagline: 'Framework for building web3 apps',
-  favicon: 'img/HypergraphLogo.png',
 
   // Set the production url of your site here
   url: 'https://graphprotocol.github.io',
