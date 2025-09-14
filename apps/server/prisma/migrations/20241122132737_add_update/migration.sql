3055fe529148e74202133863d868c0e09ed71db9
Nik Graf <nik@nikgraf.com>
2025-09-14 19:44:20 +0200

    remove old server

--- apps/server/prisma/migrations/20241122132737_add_update/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Update" (
-    "spaceId" TEXT NOT NULL,
-    "clock" INTEGER NOT NULL,
-    "content" BLOB NOT NULL,
-
-    PRIMARY KEY ("spaceId", "clock"),
-    CONSTRAINT "Update_spaceId_fkey" FOREIGN KEY ("spaceId") REFERENCES "Space" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
--- apps/server/prisma/migrations/20241212164639_add_session_nonce_token/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Account" ADD COLUMN "sessionNonce" TEXT;
-ALTER TABLE "Account" ADD COLUMN "sessionToken" TEXT;
-ALTER TABLE "Account" ADD COLUMN "sessionTokenExpires" DATETIME;
