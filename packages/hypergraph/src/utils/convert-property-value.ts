436fe4cbe1dcdf574be5169f6d7a6347fdcd3b15
Nik Graf <nik@nikgraf.com>
2026-01-15 11:40:39 +0100

    add more fallbacks

--- packages/hypergraph/src/utils/convert-property-value.ts
+++ packages/hypergraph/src/utils/convert-property-value.ts
@@ -12,23 +12,44 @@ export const convertPropertyValue = (
       return property.string;
     }
     if (propertyType.value === 'boolean') {
-      return Boolean(property.boolean);
+      // Handle case where number is stored as string in the API
+      if (property.boolean != null) {
+        return Boolean(property.boolean);
+      }
+      if ((property.string != null && property.string === '1') || property.string === '0') {
+        return Boolean(property.string);
+      }
+      return undefined;
     }
     if (propertyType.value === 'point') {
-      return property.point;
+      // Handle case where point is stored as string in the API
+      if (property.point != null) {
+        return property.point;
+      }
+      if (property.string != null) {
+        return property.point;
+      }
+      return undefined;
     }
     if (propertyType.value === 'number') {
       // Handle case where number is stored as string in the API
       if (property.number != null) {
         return Number(property.number);
       }
-      if (property.string != null) {
+      if (property.string != null && property.string !== '' && !Number.isNaN(Number(property.string))) {
         return Number(property.string);
       }
       return undefined;
     }
     if (propertyType.value === 'date') {
-      return property.time;
+      // Handle case where date is stored as string in the API
+      if (property.time != null) {
+        return new Date(property.time);
+      }
+      if (property.string != null && property.string !== '') {
+        return new Date(property.string);
+      }
+      return undefined;
     }
   }
 };
